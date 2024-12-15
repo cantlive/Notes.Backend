d685221e6a614bcec7467c5e7f1a055732a33ee8
kreativniynick <kreativniynick@gmail.com>
2024-12-16 02:18:17 +0600

    Init

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatinDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
