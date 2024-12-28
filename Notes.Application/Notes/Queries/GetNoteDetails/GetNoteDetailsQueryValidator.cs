a2733b3c3e648ee017de90de0672f377405092e0
kreativniynick <kreativniynick@gmail.com>
2024-12-28 17:52:28 +0600

    Add commands and queries validation

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteDetailsQueryValidator : AbstractValidator<GetNotesDetailsQuery>
+    {
+        public GetNoteDetailsQueryValidator()
+        {
+            RuleFor(note => note.Id).NotEqual(Guid.Empty);
+            RuleFor(note => note.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryValidator : AbstractValidator<GetNotesDetailsQuery>
+    {
+        public GetNoteListQueryValidator()
+        {
+            RuleFor(note => note.Id).NotEqual(Guid.Empty);
+            RuleFor(note => note.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
